cc18c7f3d9dfa25edce908613916dab74233e001
RYDAHXYBER TEAM <111449068+RydahXyber@users.noreply.github.com>
2022-09-27 05:40:45 +0800

    Update NewIGproRyd.py

--- NewIGproRyd.py
+++ NewIGproRyd.py
@@ -327,7 +327,7 @@ def login_kamu():
 
              ___userid = re.search('ds_user_id=(.*?);', ___cookie);open('.username','w').write(___userid.group(1))
              ___roz = requests.get(f'https://i.instagram.com/api/v1/users/{___userid.group(1)}/info/', headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.198 Mobile Safari/537.36 Instagram 166.1.0.42.245 Android (29/10; 420dpi; 1080x2042; samsung; SM-G973F; beyond1; exynos9820; en_GB; 256099204)', 'cookie':cok}).json()['user'];open('.kukis.log','w').write(cok)
-	#        os.system('clear')
+	     os.system('clear')
              print(f"{T}╚═➣{U} Welcome :{J} {___roz['full_name']}");___follow___()
         except (AttributeError, KeyError):
             exit(f"{T}╚═➣{M} COOKIE ANDA KADARLUARSA SILAHKAN PERBARUI TERLEBIH DAHULU !!!")
