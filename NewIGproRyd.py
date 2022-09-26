f4dc6b31471a8e1d3ff51ae9febd28fe89da13af
RYDAHXYBER TEAM <111449068+RydahXyber@users.noreply.github.com>
2022-09-27 05:20:54 +0800

    Update NewIGproRyd.py

--- NewIGproRyd.py
+++ NewIGproRyd.py
@@ -325,28 +325,14 @@ def login_kamu():
 		
         try:
 
-           c=s.get("https://i.instagram.com/api/v1/users/?username={"username":user}/info/",headers={"user-agent":USN,"x-ig-app-id":'936619743392459',"cookie":kuki})
-
-           i=c.json()["data"]["user"]
-
-           nama=i["full_name"]
-
-           followers=i["edge_followed_by"]["count"]
-
-           following=i["edge_follow"]["count"]
-
-           external.append(f'{nama}|{followers}|{following}')
-
-        except  (ValueError,KeyError):
-
-           cetak(nel('[white] Gagal masuk/login', style='green'))
-
-           time.sleep(4)
-
-           os.remove('.kukis.log')
-
-           os.remove('.username')
-		
+             ___userid = re.search('ds_user_id=(.*?);', ___cookie);open('.username','w').write(___userid.group(1))
+             ___roz = requests.get(f'https://i.instagram.com/api/v1/users/{___userid.group(1)}/info/', headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.198 Mobile Safari/537.36 Instagram 166.1.0.42.245 Android (29/10; 420dpi; 1080x2042; samsung; SM-G973F; beyond1; exynos9820; en_GB; 256099204)', 'cookie':cok}).json()['user'];open('.kukis.log','w').write(cok)
+	os.system('clear')
+            print(f"{T}╚═➣{U} Welcome :{J} {___roz['full_name']}");___follow___()
+        except (AttributeError, KeyError):
+            exit(f"{T}╚═➣{M} COOKIE ANDA KADARLUARSA SILAHKAN PERBARUI TERLEBIH DAHULU !!!")
+        except (ConnectionError):
+            exit(f"{T}[{J}!{T}]{J} Connection Error")	
 #        elif loginpil == '2':
 
 #                login()
